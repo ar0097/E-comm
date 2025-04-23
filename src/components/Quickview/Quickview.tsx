4efbba0f4c79e9fcdeb151cd0560a1ae4c45394f
Arbaz Shah <arbazshah7454@gmail.com>
2025-04-23 12:14:08 +0530

    responsive quick view

--- src/components/Quickview/Quickview.tsx
+++ src/components/Quickview/Quickview.tsx
@@ -31,7 +31,7 @@ function Quickview({ index, setIndex }: Indexes) {
     dispatch(addToCart(data));
   };
   return (
-    <div className="fixed z-50 top-32 left-96 w-[50%] h-[65%] border-2 bg-white over">
+    <div className="fixed z-50 top-32 md:left-96 left-12 md:w-[50%] md:h-[65%] w-[80%] h-[55%] border-2 bg-white">
       <div className="relative">
         <button
           className="absolute top-3  text-[30px] right-5 text-rose-500"
@@ -44,21 +44,24 @@ function Quickview({ index, setIndex }: Indexes) {
           />
         </button>
       </div>
-      <div className="flex items-center mt-3">
-        <div className="w-[40%] h-96 m-5">
+      <div className="md:flex items-center mt-3">
+        <div className="md:w-[40%] w-[80%] md:h-96 h-72 m-auto md:m-5">
           <img
             src={product[index].image}
             alt=""
             className="w-full h-full object-contain"
           />
         </div>
-        <div className="w-1/2 space-y-3">
+        <div className="md:w-1/2 space-y-3 ml-6">
           <p className="font-semibold text-[20px]">{product[index].type}</p>
-          <h1 className="font-bold text-[25px]/[30px]">
+          <h1 className="font-bold md:block hidden text-[25px]/[30px]">
             {product[index].name}
           </h1>
+          <h1 className="font-bold text-[25px]/[30px]">
+            {product[index].name.slice(0,20)}
+          </h1>
           <p className="font-bold text-[20px]">₹{product[index].price}</p>
-          <p className="tracking-wider">
+          <p className="tracking-wider md:block hidden">
             At Our Shop, we bring you a curated selection of{" "}
             <strong>high-quality products</strong> ranging from{" "}
             <strong>electronics, fashion, accessories, and more</strong>. Each
